38f2eff890477a207d2e20e48f88c7b02d2021e8
fernandocomet <fernandocomet@gmail.com>
2020-04-01 20:07:39 +0200

    data.json

---
+++ geg/src/Bigphoto.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Bigphoto extends Component{
+
+    render(){
+        return(
+            <h1>Here the photo</h1>
+        )
+    }
+}
+
+export default Bigphoto;
---
+++ geg/src/Quizz.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Bigphoto from './Bigphoto';
+//import PostData from './data/images.json';
+import axios from "axios";
+
+class Quizz extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {images:[]}
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+    
+        axios
+          .get("./data/images.json")
+          .then(images => {
+            this.setState({
+              ...this.state,
+              images: images.data
+            });
+          });
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Quizz</h1>
+                <Bigphoto />
+                {/* <{PostData.map((item, idx) => {
+                  return <div>
+                            <h1>Item</h1>
+                            <p>{item.Country}</p>
+                            <p>{item.ImageURL}</p>
+                            <p>{item.ID}</p>
+                         </div>
+                })} /> */}
+                <ul>
+                    {this.state.images.map((item, idx) => (
+                        <li key={idx}>{item.country} {item.ImageURL} {item.id}</li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Quizz;
+
+// {"Country":"Australia","Region":"-","ImageURL":"www.gstatic.com/prettyearth/assets/full/1003.jpg","GoogleMapsURL":"www.google.com/maps/@-10.040181,143.560709,12z/data=!3m1!1e3","ID":1003},
+
+{/* <div >
+        <h1>Hello There</h1>
+        {postList.map((item, index) => {
+          return <PostDetail
+            post={item}
+            key={`post-list-key ${index}`}
+            didHandleRemove={this.handlePostRemove}
+            dataCallback={this.handleDataCallback} />
+        })}
+</div>
+
+                <div className="container">
+                    {this.state.boxesArr.map((n, idx) => (
+                        <Colorbox key={idx}/>
+                    ))}
+                 </div> */}
