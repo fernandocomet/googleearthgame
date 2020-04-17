2717a8fa74788920d36d99bcef3937a149fc68c3
fernandocomet <fernandocomet@gmail.com>
2020-04-17 10:34:02 +0200

    Fixed Restart Game

--- geg/src/Quizz.js
+++ geg/src/Quizz.js
@@ -4,6 +4,10 @@ import './Quizz.css';
 
 class Quizz extends Component{
 
+    static defaultProps = {
+      totalPlays: 20
+    } 
+
     constructor(props){
         super(props);
         this.state = 
@@ -20,12 +24,12 @@ class Quizz extends Component{
             numPlays: 0,
             rolling: false,
             quizzing: false,
-            quizButton: "START", 
-            itemStyle: '',
-            clickedStyle: '',
+            gamestarted: false,
+            quizButton: "START"
         }
         this.randomizeItem = this.randomizeItem.bind(this);
         this.initialSet = this.initialSet.bind(this);
+        this.resetGame = this.resetGame.bind(this);
     }
 
     componentDidMount() {
@@ -33,10 +37,6 @@ class Quizz extends Component{
           .then(response => response.json())
           .then(data => this.setState({ data }))
           .then(this.initialSet)
-
-        this.setState({
-          itemStyle: 'neutral'
-        })  
     }
 
     initialSet(){
@@ -47,10 +47,12 @@ class Quizz extends Component{
           let countriesArrSet = new Set(countriesArr);
           let randomItem = Math.floor(Math.random()*this.state.data.length); 
           let randomImage = this.state.data[randomItem].ImageURL;
+          let plays = 0;
 
           this.setState({
               countries : countriesArrSet,
               imageChosen: randomImage,
+              numPlays: plays
           })
     }
 
@@ -73,38 +75,39 @@ class Quizz extends Component{
         let randomImage = this.state.data[randomItem].ImageURL;
         let randomCountry = this.state.data[randomItem].Country;
         let randomId = this.state.data[randomItem].id;
+        let region = this.state.data[randomItem].Region;
+        if(region === "-"){region = 'unknown'}
+        console.log(randomCountry + ', region ' + region)
+
+        //itemChosen : {Country:'', Region:'', ImageURL:'', GoogleMapsURL:'', id:''},
 
         theChosen.push(randomId);
 
         //Random values Set countries
         let countriesArr = Array.from(this.state.countries);
+
         countriesArr = countriesArr.filter((item) => {return item !== this.state.countryChosen })
-       
         let random1 = Math.floor(Math.random()*countriesArr.length);
         let random2 = Math.floor(Math.random()*countriesArr.length);
         let random3 = Math.floor(Math.random()*countriesArr.length);
-
+        
         countriesArr = countriesArr.filter((item) => { return item !== countriesArr[random1]})
         countriesArr = countriesArr.filter((item) => { return item !== countriesArr[random2]})
         countriesArr = countriesArr.filter((item) => { return item !== countriesArr[random3]})
 
         let countriesQuizz4 =[];
-        //console.log('this.state.countriesQuizz = ' + this.state.countriesQuizz);
-        const countriesQuizzNew = this.state.countriesQuizz;
-        console.log(countriesQuizzNew);
-
-        //let countryChosen = this.state.countryChosen;
         countriesQuizz4.push(randomCountry) 
         countriesQuizz4.push(countriesArr[random1])
         countriesQuizz4.push(countriesArr[random2])
         countriesQuizz4.push(countriesArr[random3]);
 
+        // This is to fix the Bug, write me if you found a better solution 
+        for(let i=0 ; i<countriesQuizz4.length; i++){
+          if(countriesQuizz4[i] === undefined){countriesQuizz4[i] = "Transnistria"} //There´s good wine there ;D
+        }
+
         this.shuffleArray(countriesQuizz4);
 
-        let numPlays = this.state.numPlays;
-        numPlays = numPlays +1;
-
-        let newItemStyle = 'neutral';
         
         this.setState({
           countriesQuizz: countriesQuizz4, 
@@ -116,83 +119,91 @@ class Quizz extends Component{
           chosenItems: theChosen,
           rolling : true,
           quizzing: false,
-          numPlays: numPlays,
-          itemStyle: newItemStyle,
-          clickedStyle: newItemStyle 
+          gamestarted: true
         });
     }
 
     // TO DO
-    // Init and End Gaame
+    // Preload  https://commons.wikimedia.org/wiki/Category:Google_SVG_logos   https://thenounproject.com/term/camera/3267485/ Created by Vincencio from the Noun Project
+    // Layer place, aciertos/score
+    // Mobile
+    // Twit
+
   
 
     handleChange(idx, event) {
-      console.log(idx);
+      //console.log(idx);
       let correctAnswer;
 
       for (let i=0 ; i<this.state.countriesQuizz.length; i++){
-          console.log(this.state.countriesQuizz[i] + " - " + i);
+          //console.log(this.state.countriesQuizz[i] + " - " + i);
           if(this.state.countryChosen === this.state.countriesQuizz[i]){
-            console.log("the correct answer is " + i);
+            //console.log("the correct answer is " + i);
             correctAnswer = i;
           }
       }
 
       let score = this.state.score;
-      let clickedStyleRight = 'right';
-      let clickedStyleWrong = 'wrong';
 
       if(idx === correctAnswer){
-          console.log("OK!");
-          this.setState({clickedStyle: clickedStyleRight })
-          event.target.className = this.state.clickedStyle;
-          //event.target.className = 'right'; 
+          //console.log("OK!");
           //event.target.className = 'collection-item liitem teal accent-3';
           score = score +1;
       }else{
-          console.log("WRONG!");
-          this.setState({clickedStyle: clickedStyleWrong })
-          event.target.className = this.state.clickedStyle;
-          //event.target.className = 'wrong';
+          //console.log("WRONG!");
           //event.target.className = 'collection-item liitem red darken-1';
-          //Style correctAnswer TO DO
       }
 
+      let numPlays = this.state.numPlays;
+      numPlays = numPlays +1;
+
+      if(numPlays === this.props.totalPlays){this.setState({ rolling: true })}
+      else{this.setState({ rolling: false })}
 
       this.setState({
         score: score,
-        rolling: false,
         quizzing: true,
-        quizButton: "NEXT"
-        //itemStyle: newItemStyle
+        quizButton: "NEXT",
+        numPlays: numPlays
       })
     }
 
     resetGame(){
-      console.log("Reset");
+      //console.log("Reset");
+      let theChosenReset = [];
+      let numPlaysReset = 0;
+
+      this.setState({
+        chosenItems: theChosenReset,
+        rolling : true,
+        quizzing: false,
+        numPlays: numPlaysReset
+      })
+
     }
 
 
-
     render(){
-      let leftPlays = 10 - this.state.numPlays;  //TO DO change 10 for variable
+      let leftPlays = this.props.totalPlays - this.state.numPlays; 
       let introEnd;
 
-      if(this.state.numPlays < 1){
+      if(this.state.gamestarted === false){           
           introEnd = <h5 className="introEnd">Find out where the photo was taken. Select one of the four possible answers.</h5>
-      }else if(this.state.numPlays >= 10){
-          introEnd = 
-          <div className="introEnd">
-              <h5>You scored {this.state.score} out of {this.state.numPlays}</h5>
-              <button className="waves-effect blue darken-2 btn next again" onClick={this.resetGame} >PLAY AGAIN</button>
-          </div>
-      }else{
-          introEnd = 
-          <div>
-                {this.state.countriesQuizz.map((item, idx) => (
-                      <button disabled={this.state.quizzing} className={this.state.itemStyle} onClick={this.handleChange.bind(this, idx)} key={idx}>{item}</button>
-                ))}
-          </div>
+      }else{ 
+          if(this.state.numPlays === this.props.totalPlays){
+            introEnd = 
+            <div className="introEnd">
+                <h5>You scored {this.state.score} out of {this.state.numPlays}</h5>
+                <button className="waves-effect blue darken-2 btn next again" onClick={this.resetGame} >PLAY AGAIN</button>
+            </div>
+        }else{
+            introEnd = 
+            <ul className="collection multiple-choice">
+                  {this.state.countriesQuizz.map((item, idx) => (
+                        <button disabled={this.state.quizzing} className="neutral" onClick={this.handleChange.bind(this, idx)} key={idx}>{item}</button>
+                  ))}
+            </ul>
+        }
       }
 
       return(
@@ -214,6 +225,11 @@ class Quizz extends Component{
 export default Quizz;
 
 /*
+          <div>
+                {this.state.countriesQuizz.map((item, idx) => (
+                      <button disabled={this.state.quizzing} className={this.state.itemStyle} onClick={this.handleChange.bind(this, idx)} key={idx}>{item}</button>
+                ))}
+          </div>
 {"Country":"Australia","Region":"-","ImageURL":"https://www.gstatic.com/prettyearth/assets/full/1003.jpg","GoogleMapsURL":"https://www.google.com/maps/@-10.040181,143.560709,12z/data=!3m1!1e3","id":1003},
 <button className={"waves-effect blue darken-2 btn next"} onClick={this.randomizeItem} disabled={this.state.rolling}>{this.state.quizButton}</button>             
 itemStyle
